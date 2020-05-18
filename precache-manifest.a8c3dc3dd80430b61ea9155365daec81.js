280e61e4830723a30ef47461f13c8228d17c1cd3
Joydeep Roy <joydeeprox06@gmail.com>
2020-05-18 23:31:29 +0530

    Updates

---
+++ precache-manifest.a8c3dc3dd80430b61ea9155365daec81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea196ae2a25c65d47c8ac54762b0847d",
+    "url": "/Joydeep06/robofriends/index.html"
+  },
+  {
+    "revision": "626b9655b6fe2d83b7a8",
+    "url": "/Joydeep06/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "05f68b478566026006e4",
+    "url": "/Joydeep06/robofriends/static/css/main.b8fd0c6d.chunk.css"
+  },
+  {
+    "revision": "626b9655b6fe2d83b7a8",
+    "url": "/Joydeep06/robofriends/static/js/2.383251a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Joydeep06/robofriends/static/js/2.383251a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f68b478566026006e4",
+    "url": "/Joydeep06/robofriends/static/js/main.94f9ff43.chunk.js"
+  },
+  {
+    "revision": "e270dbe520c03713d357",
+    "url": "/Joydeep06/robofriends/static/js/runtime-main.3443dc0d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Joydeep06/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
